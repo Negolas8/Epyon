fd504f07eddbf61be32f9f299779b0cc70cc95e2
Negolas8 <nicolas.constantinov.nogueira@escola.pr.gov.br>
2026-04-07 11:54:10 +0000

    versao3

--- soma.js
+++ soma.js
@@ -1,8 +1,15 @@
 function somar(numero1, numero2) {
     return numero1 + numero2
 }
+
 console.log(somar(5,2 ));
 
-funtion verificarPar(numero) {
-    if ()
-}
+function verificarPar(numero) {
+    if (numero % 2 === 0) {
+        "O número é par"
+    } else {
+        return "O número é impar"
+    }
+};
+
+console.log(verifivarPar(11));

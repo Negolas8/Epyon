b49a84e6a6685b7785a2ff488515968648892cc7
Negolas8 <nicolas.constantinov.nogueira@escola.pr.gov.br>
2026-04-28 08:37:07 -0300

    Add files via upload

---
+++ Aluno.js
@@ -0,0 +1,17 @@
+aluno.js
+
+const alunos = ["Ana", "Bruna", "Pietra"];
+
+alunos.push("Wesley"); //Adiciona elemento no final do array
+alunos.unshift("Alan")
+console.log(alunos);
+
+const removido = alunos.pop();
+console.log(removido);
+console.log(alunos);
+
+console.log(alunos.indexOf("Maria"));
+console.log(alunos.indexOf("Pedro"));
+
+console.log(alunos.includes("Ana"));
+console.log(alunos.includes("Guilherme"));
